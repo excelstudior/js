be9e0b1aa0d5f8926a4af84b589878a41a04060d
Ray Yang <>
2019-07-01 21:33:49 +1000

    add commit

--- src/AppRedux/Apps/TodoList/Todo.js
+++ src/AppRedux/Apps/TodoList/Todo.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
 import {changePendingTodoValue,addTodo,removeTodo} from './action'
-
+//function component improve performance, and it won't run life cycle method
 const Todo=({index,todo,removeTodo})=>{
     return <li key={index+todo} value={index} onClick={removeTodo}>{todo}</li>
 }
-
-class TodoList extends Component {
-    constructor(props){
-        super(props)
-    }
-
-    render() { 
-        console.log(this.state)
-        const {todos,pendingTodo,removeTodo}=this.props;
+const TodoList=(props)=>{
+    const {todos,pendingTodo,removeTodo,changePendingTodo,addTodo}=props;
         return (  
             <div>
                 <input value={pendingTodo} placeholder='new todo'
-                       onChange={this.props.changePendingTodo}/>
-                <button onClick={this.props.addTodo}>Add</button>
+                       onChange={changePendingTodo}/>
+                <button onClick={addTodo}>Add</button>
                 {todos.length>0?<ul>
                     {todos.map((todo,index)=>{
                            return <Todo index={index} 
@@ -29,8 +22,32 @@ class TodoList extends Component {
                 </ul>:<br/>}
             </div>
         )
-    }
 }
+// class TodoList extends Component {
+//     constructor(props){
+//         super(props)
+//     }
+
+//     render() { 
+//         console.log(this.state)
+//         const {todos,pendingTodo,removeTodo,changePendingTodo,addTodo}=this.props;
+//         return (  
+//             <div>
+//                 <input value={pendingTodo} placeholder='new todo'
+//                        onChange={changePendingTodo}/>
+//                 <button onClick={addTodo}>Add</button>
+//                 {todos.length>0?<ul>
+//                     {todos.map((todo,index)=>{
+//                            return <Todo index={index} 
+//                                         todo={todo}
+//                                         removeTodo={removeTodo}/>
+//                     })}
+                    
+//                 </ul>:<br/>}
+//             </div>
+//         )
+//     }
+// }
 //state as parameter and return and object
 const mapStateToProps=(state)=>{
     return{
