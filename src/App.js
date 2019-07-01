08d10037a863470596a47be79583e7db0c98c92e
Ray Yang <>
2019-07-01 22:35:42 +1000

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component,Fragment} from 'react';
 import {ResetCss} from './style.js'
+import Header from './common/header/index';
 class App extends Component {
     state = {  }
     render() { 
         return ( 
            <Fragment>
                <ResetCss/>
+               <Header/>
                <div>Hello Jianshu</div>
            </Fragment>
             
