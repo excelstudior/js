b9d09d8e1df47440f00bd09fd177d44b0294d076
Ray Yang <>
2019-07-01 21:48:45 +1000

    Clean up

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Hello Jianshu</div>
+         )
+    }
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import Rose from './img/rose.jpg';
-//import PersonalService from './personalService';
-import './css/index.css';
-//import List from './List'
-import AppRedux from './AppRedux/AppRedux'
-// class App extends React.Component{
-//     state = {
-//         reset:'yes'
-//     }
-//     onClick=()=>{
-//         this.setState({
-//             reset:this.state.reset==='yes'?'no':'yes'
-//         })
-//     }
-//     render(){
-//         return(
-//             <div>
-//                 <img src={Rose} alt='rose' className={this.state.reset==='yes'?'flower':'flower flower-rotate'} onClick={this.onClick}/>
-//                 <PersonalService/>
-//                 <List/>
-//             </div>
-//         )
-//     }
-// }
-//ReactDOM.render(<App/>,document.getElementById('root'))
-ReactDOM.render(<AppRedux/>,document.getElementById('root'))
+
+ReactDOM.render(<App/>,document.getElementById('root'))
