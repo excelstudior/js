174169c3f28b587192b0043b670ba79c47b133e0
Ray Yang <ryang@wfsaustralia.com>
2019-07-12 20:16:07 +1000

    add optimize css asset plugin and global

--- src/common/signUp/index.js
+++ src/common/signUp/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import $ from 'jquery'
 
+const a=[1,2,3]
+const b=[...a]
+const c=b.includes(2)
 const signUp=()=>{
+    console.log($)
     return(
         <div>Sign me up</div>
     )
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,20 @@ const path=require('path');
 const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const webpack=require('webpack');
 module.exports={
-    entry:'./src/index.js',
+    optimization:{
+        minimizer:[
+            new OptimizeCssAssetsPlugin(),
+            new UglifyJsPlugin({
+                cache:true,
+                sourceMap:true
+            })
+        ]
+    },
+    entry:['@babel/polyfill','./src/index.js'],
     output:{
         path:path.resolve(__dirname,"dist"),
         filename:'bundle.[hash:8].js'
@@ -11,7 +23,7 @@ module.exports={
     module:{
         rules:[{
             test:/\.css$/,
-            use:[ MiniCssExtractPlugin.loader,'css-loader'],//start from right
+            use:[MiniCssExtractPlugin.loader,'css-loader'],//start from right
             include:path.resolve(__dirname,'public/css'),
             exclude:/node_modules/
         }, {
@@ -22,7 +34,10 @@ module.exports={
             loader:'babel-loader',
             options:{
                 presets:['@babel/preset-env','@babel/preset-react'],
-                plugins:['@babel/plugin-proposal-class-properties']
+                plugins:['@babel/plugin-proposal-class-properties',
+                "@babel/plugin-transform-runtime",
+                "@babel/plugin-transform-spread"
+            ]
             },
             exclude: /node_modules/
         },{
@@ -47,6 +62,9 @@ module.exports={
         }),
         new MiniCssExtractPlugin({
             filename:'main.css'
+        }),
+        new webpack.ProvidePlugin({
+            '$':'jquery'
         })
     ],
     devServer:{
